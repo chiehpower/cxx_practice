65198035dc18c0d7d1f2648c00df96d9fef010e4
chiehpower <iloveberit@gmail.com>
2020-01-09 17:26:52 +0800

    Start to practice c++

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# cxx_practice
-c++ practice
+# C++ practice
+Only for c++ practice
+1. Day1.cpp
+```
+g++ day1.cpp -o day1.out -W
+```
+ 
+
---
+++ day1.cpp
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+int main(void)
+{
+
+for (int i=0; i<=10;i++){
+cout << "=";
+}
+cout << "\n";
+cout << "\n";
+cout << "hello";
+cout << "\n";
+return 0;
+
+}
+
